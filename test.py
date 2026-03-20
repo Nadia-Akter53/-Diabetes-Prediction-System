115950b82e1afb0ddea6949d96c40591cdcfbe1b
Nadia Akter Eshita <eshitasundor@gmail.com>
2026-03-21 05:09:43 +0600

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Test", layout="wide")
-
-st.title("Diabetes Prediction System")
-st.write("If you see this, Streamlit is working!")
-
-# Simple test to check if it loads
-st.success("✅ App is running!")
